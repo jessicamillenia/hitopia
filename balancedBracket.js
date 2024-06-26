05b3d4c10e02ec8d88807936552fa7c042251c58
jessicamillenia <73098443+jessicamillenia@users.noreply.github.com>
2024-06-26 11:47:26 +0700

    Create balancedBracket.js

---
+++ balancedBracket.js
@@ -0,0 +1,27 @@
+function isBalancedBracket(brackets) {
+    const checkIsBalaced = [];
+    const openingBracket = ['{', '[', '('];
+    const closingBracket = ['}', ']', ')'];
+    const mapClosingBracket = {
+        ']': '[',
+        ')': '(',
+        '}': '{'
+    };
+
+    for (let i=0;i<brackets.length;i++) {
+        if (openingBracket.includes(brackets[i])) {
+            checkIsBalaced.push(brackets[i]);
+        } else if (closingBracket.includes(brackets[i])) {
+            if (checkIsBalaced.length === 0 || checkIsBalaced[checkIsBalaced.length - 1] !== mapClosingBracket[brackets[i]]) {
+                return "NO";
+            }
+            checkIsBalaced.pop();
+        }
+    }
+
+    return checkIsBalaced.length === 0 ? "YES" : "NO";
+}
+
+console.log(isBalancedBracket("{ [ ( ) ] }"));
+console.log(isBalancedBracket("{ [ ( ] ) }"));
+console.log(isBalancedBracket("{ ( ( [ ] ) [ ] ) [ ] }"));
